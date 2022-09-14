9726c9305c362e5a8887d86f331539c5a5b6d4e5
Logan Hall <logan.hall@student.awesomeinc.org>
2022-09-14 18:54:37 +0000

    added two arrays one for imgs and the second for their  alt text.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,21 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const images = [
+    'img/pic1.jpg',
+    'img/pic2.jpg',
+    'img/pic3.jpg',
+    'img/pic4.jpg',
+    'img/pic5.jpg'
+]
 /* Declaring the alternative text for each image file */
-
+const alts = {
+    'img/pic1.jpg' : 'Closeup of a human eye',
+    'img/pic2.jpg' : 'Rock that looks like a wave',
+    'img/pic3.jpg' : 'Purple and white pansies',
+    'img/pic4.jpg' : 'Section of wall from a pharoahs tomb',
+    'img/pic5.jpg' : 'Large moth on a leaf'
+}
 /* Looping through images */
 
 const newImage = document.createElement('img');
